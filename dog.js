d18089c76cff58b198b4124fff0fdc76f91f9e49
hunter <hunter@hunter.net.fpt>
2019-12-10 14:55:54 +0700

    hide file modules

--- dog.js
+++ dog.js
@@ -1,3 +1,14 @@
-function dog(){
-    
+var chalk = require ('chalk');
+function dog(name){
+    this.name = name;
+    this.stomatch = [];
 }
+
+dog.prototype.eat = function (cat){
+    this.stomatch(cat);
+}
+
+dog.prototype.sayHi = function (){
+    console.log('Hi i am a dog. My name is ' + chalk.red(this.name));
+}
+module.exports = dog;
