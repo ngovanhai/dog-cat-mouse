5a03f436cba7f2c1aba8d0abf4265543f00e4651
hunter <hunter@hunter.net.fpt>
2019-12-10 22:15:50 +0700

    cat eat method

--- cat.js
+++ cat.js
@@ -1,7 +1,13 @@
+var mouse = require('./mouse');
 function cat() {
 	this.stomach = [];
 }
-cat.prototype.eat = function(mouse){
-	this.stomach.push(mouse);
+cat.prototype.eat = function(animal){
+	if(animal instanceof mouse){
+		this.stomach.push(animal);
+	}else{
+		throw new Error('Cat can only muose !');
+	}
+	
 }
 module.exports = cat;
